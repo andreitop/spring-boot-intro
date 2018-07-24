70cb927ece3311c3fc1fca46c58ceb7dc28caaf9
Andrei Dremov <andrei_dremov@epam.com>
2018-07-24 18:28:57 +0300

    autoconf-3 | Step-6 | Conditional annotations

--- src/main/java/com/andreitop/springbootintro/MongoInfoLog.java
+++ src/main/java/com/andreitop/springbootintro/MongoInfoLog.java
@@ -2,12 +2,16 @@ package com.andreitop.springbootintro;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import javax.annotation.PostConstruct;
 
 @Configuration
+@ConditionalOnClass(MongoRepository.class)
+@ConditionalOnProperty(prefix = "prod.mongodb", name = "log", havingValue = "true")
 public class MongoInfoLog {
 
     private final Logger logger = LoggerFactory.getLogger(MongoInfoLog.class);
