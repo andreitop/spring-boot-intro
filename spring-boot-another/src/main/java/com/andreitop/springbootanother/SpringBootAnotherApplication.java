8ef966707ce01de6f115093c4b98bcf52b2b0dd9
Andrei Dremov <andrei_dremov@epam.com>
2018-07-24 18:21:52 +0300

    autoconf-2 | Step-5 | Auto-configuration problem with transitive dependencies

---
+++ spring-boot-another/src/main/java/com/andreitop/springbootanother/SpringBootAnotherApplication.java
@@ -0,0 +1,12 @@
+package com.andreitop.springbootanother;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAnotherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAnotherApplication.class, args);
+    }
+}
